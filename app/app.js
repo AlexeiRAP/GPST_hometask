69a02c094d2323dddf1a5061881736f76aff4083
AlexeiRAP@gmail.com <AlexeiRAP@gmail.com>
2016-02-06 14:06:36 +0300

    GPST_HT2: create my own directive and input calc logic inside the directive

--- app/app.js
+++ app/app.js
@@ -1,9 +1,10 @@
 var HomeTaskApp = angular.module("HomeTask", []);
 
 HomeTaskApp.controller("MainCtrl", ["$scope", function($scope){
-	$scope.num1 = "Sum";
-	$scope.num2 = "ma";
+	$scope.num1 = 1;
+	$scope.num2 = 2;
 	
+	/*
 	var listenerFun = function(newValue, oldValue, $scope){
 		if (isFinite($scope.num1) == true && isFinite($scope.num2) == true) {
 			$scope.num3 = +$scope.num1 + +$scope.num2;
@@ -15,5 +16,29 @@ HomeTaskApp.controller("MainCtrl", ["$scope", function($scope){
 	}
 	$scope.$watch('num1',listenerFun);
 	$scope.$watch('num2',listenerFun);
+	*/
+}]);
 
-}])
+
+HomeTaskApp.directive("raDirective", function(){
+
+	return {
+		restrict: 'AE',
+    	link: function($scope){
+    		
+    		var listenerFun = function(newValue, oldValue, $scope){
+				if (isFinite($scope.num1) == true && isFinite($scope.num2) == true) {
+					$scope.num3 = +$scope.num1 + +$scope.num2;
+					//alert("введённые значения - числа");
+				} else{
+					$scope.num3 = $scope.num1 + $scope.num2;
+					//alert("введённые значения - не числа");	
+				};	
+			};	
+			$scope.$watch('num1',listenerFun);
+			$scope.$watch('num2',listenerFun);
+    	},
+    	//templateUrl: 'app/directives.html',
+    	template: "<div><label>Summa =</label><input value={{num3}}></div>"
+    };
+});
