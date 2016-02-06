0ee6b5269758e18fd3e2225113555d9d130532eb
AlexeiRAP@gmail.com <AlexeiRAP@gmail.com>
2016-02-06 18:28:44 +0300

    GPST_HT2: routing was added with full functionality

--- app/app.js
+++ app/app.js
@@ -1,29 +1,19 @@
-var HomeTaskApp = angular.module("HomeTask", []);
+var HomeTaskApp = angular.module("HomeTask", ["ngRoute"]);
 
 HomeTaskApp.controller("MainCtrl", ["$scope", function($scope){
-	$scope.num1 = 1;
-	$scope.num2 = 2;
+	$scope.num1 = "1";
+	$scope.num2 = "2";
 	$scope.numbers = [
 		{name: "number1", value: $scope.num1},
 		{name: "number2", value: $scope.num2},
 		{name: "number3", value: $scope.num3}
-	] 
-	
-	
-	/*
-	var listenerFun = function(newValue, oldValue, $scope){
-		if (isFinite($scope.num1) == true && isFinite($scope.num2) == true) {
-			$scope.num3 = +$scope.num1 + +$scope.num2;
-			//alert("введённые значения - числа");
-		} else{
-			$scope.num3 = $scope.num1 + $scope.num2;
-			//alert("введённые значения - не числа");	
-		}	
-	}
-	$scope.$watch('num1',listenerFun);
-	$scope.$watch('num2',listenerFun);
-	*/
+	]; 
 }]);
+
+HomeTaskApp.controller("InputCtrl", ["$scope", function($scope){
+	$scope.numbers[0].value = $scope.num1;
+	$scope.numbers[1].value = $scope.num2;
+}])
 
 
 HomeTaskApp.directive("raDirective", function(){
@@ -62,3 +52,13 @@ HomeTaskApp.directive("raDirective", function(){
 });
 
 
+HomeTaskApp.config(function($routeProvider){
+    $routeProvider
+    	.when('/:numbers', {
+    		//templateUrl: 'input/input.html',
+    		controller: 'InputCtrl'
+    	})
+    	.otherwise({ 
+    		redirectTo: '/'
+    	});
+});
