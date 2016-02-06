57f631adea989c78bf7bba7d00e52d8f15da8efc
AlexeiRAP@gmail.com <AlexeiRAP@gmail.com>
2016-02-06 16:17:28 +0300

    GPST_HT2: create model with inputs value

--- app/app.js
+++ app/app.js
@@ -3,6 +3,12 @@ var HomeTaskApp = angular.module("HomeTask", []);
 HomeTaskApp.controller("MainCtrl", ["$scope", function($scope){
 	$scope.num1 = 1;
 	$scope.num2 = 2;
+	$scope.numbers = [
+		{name: "number1", value: $scope.num1},
+		{name: "number2", value: $scope.num2},
+		{name: "number3", value: $scope.num3}
+	] 
+	
 	
 	/*
 	var listenerFun = function(newValue, oldValue, $scope){
@@ -25,20 +31,34 @@ HomeTaskApp.directive("raDirective", function(){
 	return {
 		restrict: 'AE',
     	link: function($scope){
-    		
-    		var listenerFun = function(newValue, oldValue, $scope){
+    		$scope.num3 = $scope.num1 + $scope.num2;
+    		var listenerFun = function(){//newValue, oldValue, $scope){
 				if (isFinite($scope.num1) == true && isFinite($scope.num2) == true) {
 					$scope.num3 = +$scope.num1 + +$scope.num2;
 					//alert("введённые значения - числа");
 				} else{
 					$scope.num3 = $scope.num1 + $scope.num2;
 					//alert("введённые значения - не числа");	
-				};	
+				};
+				$scope.numbers[2].value = $scope.num3;
 			};	
-			$scope.$watch('num1',listenerFun);
-			$scope.$watch('num2',listenerFun);
+			$scope.$watch('num1',function(newValue, oldValue){
+				$scope.numbers[0].value = $scope.num1;
+				listenerFun();		
+			});
+			$scope.$watch('num2',function(newValue, oldValue){
+				$scope.numbers[0].value = $scope.num1;
+				listenerFun();	
+			});
+			
+			
     	},
     	//templateUrl: 'app/directives.html',
-    	template: "<div><label>Summa =</label><input value={{num3}}></div>"
+    	template: 
+    		"<div>"+
+    		"<label>Summa =</label>"+
+    		"<input value={{num3}}></div>"
     };
 });
+
+
